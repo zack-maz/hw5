4130dff9cc8e59cc7d0cc556f937ffcdd3219614
Zack Mazaheri <dmazaher@usc.edu>
2022-11-28 22:53:02 +0000

    added comments

--- schedwork.cpp
+++ schedwork.cpp
@@ -108,7 +108,7 @@ bool schedule(
     }
     sched.clear();
 
-    std::vector<unsigned int> daysPerWorker;
+    std::vector<unsigned int> daysPerWorker; //count how many days each worker has worked
     for (unsigned int i = 0; i < avail[0].size(); i++) daysPerWorker.push_back(0);
 
     return scheduleHelper(avail, dailyNeed, maxShifts, sched, daysPerWorker);
@@ -127,31 +127,33 @@ bool scheduleHelper(
   int size = sched.size();
   if (size > 0)
   {
+    //return true if we're on the last day and we have our daily need for workers
     if (size >= avail.size() && sched[size-1].size() == dailyNeed) return true;
   }
 
   std::vector<Worker_T> nextDay;
-  if (sched.size() == 0) sched.push_back(nextDay);
-  else if (sched[sched.size()-1].size() == dailyNeed) sched.push_back(nextDay);
+  if (sched.size() == 0) sched.push_back(nextDay); //add new day schedule if our schedule is empty
+  else if (sched[sched.size()-1].size() == dailyNeed) sched.push_back(nextDay); //add new day schedule if we have enough workers for daily need
   int currentDay = sched.size()-1;
 
 
 
 
 
-  for (unsigned int i = 0; i < avail[currentDay].size(); i++)
+  for (unsigned int i = 0; i < avail[currentDay].size(); i++) //go through every worker today
   {
+    //if the worker hasn't been added, they're available. and they don't have their maxShifts
     if ((std::find(sched[currentDay].begin(), sched[currentDay].end(), i) == sched[currentDay].end()) && (avail[currentDay][i] == 1) && (daysPerWorker[i] < maxShifts))
     {
-      sched[currentDay].push_back(i);
-      daysPerWorker[i]++;
-      if (scheduleHelper(avail, dailyNeed, maxShifts, sched, daysPerWorker)) return true;
-      sched[currentDay].pop_back();
+      sched[currentDay].push_back(i); //add worker to schedule
+      daysPerWorker[i]++; //keep track of how many days theyve worked
+      if (scheduleHelper(avail, dailyNeed, maxShifts, sched, daysPerWorker)) return true; //if it passes, then this schedule works as a proper schedule
+      sched[currentDay].pop_back(); //doesnt work, check with next worker
       daysPerWorker[i]--;
     }
   }
 
-  if (sched[currentDay].size() == 0) sched.pop_back();
+  if (sched[currentDay].size() == 0) sched.pop_back(); //if we checked all workers and the sched doesn't work, pop back and try different combos for previous day
   return false;
 
 }
--- wordle.cpp
+++ wordle.cpp
@@ -20,19 +20,19 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     
-    std::deque<char> fab;
+    std::deque<char> fab; //floating char and blanks deque
     for (unsigned int i = 0; i < floating.length(); i++)
     {
-        fab.push_back(floating[i]);
+        fab.push_back(floating[i]); //push all floating points
     }
     int blanks = 0;
     for (unsigned int i = 0; i < in.length(); i++)
     {
-        if (in[i] == '-') blanks++;
+        if (in[i] == '-') blanks++; //count the number of blanks
     }
     for (unsigned int i = 0; i < blanks - floating.length(); i++)
     {
-        fab.push_back('*');
+        fab.push_back('*'); //push * for # of blanks into fab
     }
 
     std::set<std::string> answers;
@@ -52,43 +52,45 @@ void wordleHelper(
     std::set<std::string>& answers)
 {
     
-    if (match.length() >= in.length())
+    if (match.length() >= in.length()) //if our word is the right length, it could be an answer
     {
-        if (dict.find(match) != dict.end()) 
+        if (dict.find(match) != dict.end()) //if its in dict
         {
-            answers.insert(match);
-            //std::cout << match << std::endl;
+            answers.insert(match); //add to answers
             return;
         }
         else return;
     }
 
-    int index = match.length();
+    int index = match.length(); //whats our current index in the word as we're building
 
     //"ab"
     //"--c--"
 
-    if (in[index] != '-')
+    if (in[index] != '-') //fixed character, add it to match
     {
         wordleHelper(in, fab, dict, match+in[index], answers);
     }
     else
     {
-        string alph = "abcdefghijklmnopqrstuvwxyz";
+        string alph = "abcdefghijklmnopqrstuvwxyz"; //used for going through letters
         
         for (unsigned int i =0; i < fab.size(); i++)
         {
-            char c = fab.front();
+            char c = fab.front(); //check floating char or blank
             fab.pop_front();
-            if (c != '*') wordleHelper(in, fab, dict, match+c, answers);
+            if (c != '*') wordleHelper(in, fab, dict, match+c, answers); //check w/ floating char
             else
             {
+                //check any letter
                 for (unsigned int i = 0; i < alph.length(); i++) wordleHelper(in, fab, dict, match+alph[i], answers);
             }
 
-            fab.push_back(c);
-            //if (c == '*') break;
+            fab.push_back(c); //add back into fab for next char position check
         }
+
+        //basically we have x missing spots, which could either be a blank or a floating char
+        //using the deque, we check to fill in everything
 
     }
 
